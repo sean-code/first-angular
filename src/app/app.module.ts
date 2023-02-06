49979da4423bd3c969f2143571513746b712d4cc
sean-code <john.nganga@student.moringaschool.com>
2023-02-06 17:28:30 +0300

    Create footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BackgroundComponent } from './background/background.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BackgroundComponent
+    BackgroundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
